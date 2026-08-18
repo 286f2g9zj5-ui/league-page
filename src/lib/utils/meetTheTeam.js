b16ca210659533a5949cc4ac5b9b905e488b803e
286f2g9zj5-ui <286f2g9zj5@privaterelay.appleid.com>
2026-08-18 12:33:15 -0500

    Add Meet the Team owner list

---
+++ src/lib/utils/meetTheTeam.js
@@ -0,0 +1,14 @@
+export const meetTheTeamOwners = [
+  { name: 'Adam', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Cam', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Abram', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Alex', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Kevin', commissioner: true, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Dane', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Ben', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Simon', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Sam', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Taylor', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Luke', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+  { name: 'Karlen', commissioner: false, age: null, averageFinish: null, background: '', photo: null },
+];
